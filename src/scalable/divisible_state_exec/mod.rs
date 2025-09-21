845a63a307056f88f342c2677d1f3dc38b7733e0
Amadeu Marques <maramadeu@gmail.com>
2025-09-21 20:33:52 +0100

    remove debug message

--- src/scalable/divisible_state_exec/mod.rs
+++ src/scalable/divisible_state_exec/mod.rs
@@ -144,7 +144,6 @@ impl<S, A, NT> ScalableDivisibleStateExecutor<S, A, NT>
                             let seq_no = batch.sequence_number();
 
                             metric_duration(EXECUTION_LATENCY_TIME_ID, instant.elapsed());
-                            println!("STARTING CHECKPOINT");
 
                             let start = Instant::now();
 
