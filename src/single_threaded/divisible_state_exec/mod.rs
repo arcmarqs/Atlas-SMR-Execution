5bbb244d682d7205827e95a4afe74ab3eff1cf43
Amadeu Marques <maramadeu@gmail.com>
2025-09-25 22:51:11 +0100

    profiling checkpoint

--- src/single_threaded/divisible_state_exec/mod.rs
+++ src/single_threaded/divisible_state_exec/mod.rs
@@ -168,9 +168,9 @@ impl<S, A, NT> DivisibleStateExecutor<S, A, NT>
     ///Clones the current state and delivers it to the application
     /// Takes a sequence number, which corresponds to the last executed consensus instance before we performed the checkpoint
     fn deliver_checkpoint_state(&mut self, seq: SeqNo) {
-        
+        let _profiler = dhat::Profiler::new_heap();
         self.state.get_parts(&mut self.checkpoint_threadpool, self.checkpoint_tx.clone()).expect("Failed to get necessary parts");
-
+        drop(_profiler);
         let desc: AppState<S> = AppState::StateDescriptor(self.state.get_descriptor());
 
         let _ = self.checkpoint_tx.send_return(AppStateMessage::new(seq, desc));
